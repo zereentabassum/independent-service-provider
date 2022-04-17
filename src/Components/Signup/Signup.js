e415caba74aaf4da9d321bedcc09570f98920447
zereentabassum <islamms262@gmail.com>
2022-04-17 20:58:02 +0600

    sign up form created

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -6,7 +6,24 @@ const Signup = () => {
     return (
         <div className='form p-5'>
             <h1>Sign Up</h1>
-        
+            <Form className='w-25 mx-auto form-section'>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address:</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+        </Form.Text>
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label className='text-start' >Password:</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+   
+    <Button className='px-5' variant="primary" type="submit">
+        Sign up
+    </Button>
+    </Form>
         </div>
     );
 };
