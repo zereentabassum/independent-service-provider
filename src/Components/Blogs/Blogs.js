ba297b13894015f9208d073f5d9ec7fbeeb89f04
zereentabassum <islamms262@gmail.com>
2022-04-18 19:32:12 +0600

    blogs added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,11 +3,17 @@ import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div className='text-start container'>
+        <div className='text-start container flex-wrap w-75'>
             <h1 className='pt-2 pb-4 text-center'>Blogs</h1>
-            <h3><strong>Question-1: </strong>  Difference between Authorization and Authentication ?</h3>
+
+            <div className='blogs  border border-primary p-2'>
+            <h3><strong>Question-1: </strong> What is the difference between Authorization and Authentication ?</h3>
+            <p><strong>Answer: </strong>  Authentication is a process which verifies who the user is. Authorization is a process to determine which resources user can access. Authentication is visible and can be partially changable by user. Authorization is not visible and is not changable by user.</p>
             <h3><strong>Question-2: </strong> Why are you using firebase? What other options do you have to implement authentication?</h3>
-            <h3><strong>Question-3: </strong>  What other services does firebase provide other than authentication</h3>
+            <p><strong>Answer: </strong>  I am using firebase because it is fast and safe for hosting and authentication. It is easy and free to use. The other options to implement authentication are Auth0, MongoDB, Passport, Okta etc.</p>
+            <h3><strong>Question-3: </strong>  What other services does firebase provide other than authentication ?</h3>
+            <p><strong>Answer: </strong> Firebase provides many services other than authentication. These are - Cloud Firestore, Cloud Functions ,Authentication, Hosting, Cloud Storage, Google Analytics, Predictions, Cloud Messaging etc. </p>
+            </div>
         </div>
     );
 };
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import './Signup.css';
-import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth';
+import {useCreateUserWithEmailAndPassword, useUpdateProfile} from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 const Signup = () => {
@@ -15,7 +15,8 @@ const Signup = () => {
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+      ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+      const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
       const handleNameBlur = event => {
           setName(event.target.value);
@@ -27,12 +28,16 @@ const Signup = () => {
     const handlePasswordBlur = event =>{
         setPassword(event.target.value);
     }
-    const handleSignup = event => {
+    const handleSignup = async (event) => {
         event.preventDefault();
-       createUserWithEmailAndPassword(email, password);
+
+       await createUserWithEmailAndPassword(email, password);
+       await updateProfile({ displayName: name });
+       console.log('Updated profile');
+       navigate('/home');
     }
     if(user){
-        navigate('/about');
+       console.log(user);
     }
     let load;
     if (loading) {
@@ -52,7 +57,12 @@ const Signup = () => {
         <div className='form p-3'>
             <h1>Sign Up</h1>
             <Form onSubmit={handleSignup} className='w-25 mx-auto form-section'>
-            <Form.Control onBlur={handleNameBlur} type="text" placeholder="Your name" required/>
+
+            <Form.Group className='text-start'>
+            <Form.Label >Name:</Form.Label>
+            <Form.Control onBlur={handleNameBlur} type="text" 
+            placeholder="Your name" required/>
+            </Form.Group>
   <br />
     <Form.Group onBlur={handleEmailBlur} className="mb-3 text-start" controlId="formBasicEmail">
         <Form.Label>Email address:</Form.Label>
@@ -62,14 +72,14 @@ const Signup = () => {
     <Form.Group onBlur={handlePasswordBlur} className="mb-4 text-start" controlId="formBasicPassword">
         <Form.Label className='text-start' >Password:</Form.Label>
         <Form.Control type="password" placeholder="Password" required/>
-        <Form.Text className='text-success' id="passwordHelpBlock" >
+        <Form.Text className='text-success fs-6' id="passwordHelpBlock" >
     Password should be at least 6 characters or more. 
   </Form.Text>
     </Form.Group>
    
     {load}
     {errorMessage}
-    <Button className='px-5 mt-2 mb-4' variant="primary" type="submit">
+    <Button className='px-5 mb-4' variant="primary" type="submit">
         Sign up
     </Button>
     <p>Already have an account? <Link className='text-decoration-none' to='/login'>Login</Link></p>
